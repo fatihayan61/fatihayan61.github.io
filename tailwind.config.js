e92baef8fed849dac2b9721f3414113e2417bdf8
Fatih <68619021+fatihayan61@users.noreply.github.com>
2023-04-01 02:47:01 +0300

    first page

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        header: '#CDB4DB',
+        background: '#CDB4DB',
+        mypink: '#FFC8DD',
+        myboldpink: '#FFAFCC',
+        myblue: '#BDE0FE',
+        myboldblue:'#A2D2FF'
+      },
+    },
+  },
+  plugins: [],
+}
+
